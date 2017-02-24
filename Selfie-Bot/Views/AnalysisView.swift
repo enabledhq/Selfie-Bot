5e1d65a3ac1c2c34c8e6b446d59267131f1306dc
John Smith <john@johnjcsmith.com>
2017-02-24 10:51:02 +1030

    Initial AnalysisView layout

--- Selfie-Bot/Views/AnalysisView.swift
+++ Selfie-Bot/Views/AnalysisView.swift
@@ -4,6 +4,62 @@
 //
 
 import Foundation
+import SnapKit
 
-class AnalysisView {
+class AnalysisView: UIView {
+
+
+    var confidenceBars = [ConfidenceBar]()
+    
+    init(options: [String : Int]) {
+        super.init(frame: CGRect())
+        
+        backgroundColor = UIColor.white
+        
+        //Format confidence bars
+        var previousBar: UIView?
+        options.enumerated().forEach {
+            option in
+            
+            let confidenceBar = ConfidenceBar()
+            let confidenceLabel = UILabel()
+            
+            confidenceLabel.text = option.element.key
+
+
+            self.addSubview(confidenceLabel)
+            self.addSubview(confidenceBar)
+            
+            confidenceLabel.snp.makeConstraints {
+                make in
+                
+                make.height.equalTo(30)
+                if let previousBar = previousBar {
+                    make.top.equalTo(previousBar.snp.bottom).offset(10)
+                } else {
+                    make.top.equalTo(self)
+                }
+                
+                make.left.equalTo(self).inset(10)
+            }
+            
+            confidenceBar.snp.makeConstraints {
+                make in
+                
+                make.left.right.equalTo(self).inset(10)
+                make.top.equalTo(confidenceLabel.snp.bottom).offset(2.5)
+                make.height.equalTo(30)
+            }
+            previousBar = confidenceBar
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
